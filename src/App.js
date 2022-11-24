c9eb14a98eea3c490f7b4f55b4aee4b4c18ec912
Ousikai <mtfallsVR@protonmail.com>
2022-11-23 20:34:19 -0500

    🔌 Build a connect wallet button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
 //importing required libraries
 import React, { useState, useEffect } from "react";
 import './App.css';
+import * as fcl from "@onflow/fcl";
+import * as types from "@onflow/types";
+
+// Assets
 import twitterLogo from "./assets/twitter-logo.svg";
 import splashScreen from "./assets/gto6.webp"
 
-const TWITTER_HANDLE = "_buildspace";
+// Constants
+const TWITTER_HANDLE = "Atemosta";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+// FCL Config
+fcl.config({
+  "flow.network": "testnet",
+  "app.detail.title": "BottomShot", // Change the title!
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "app.detail.icon": "https://placekitten.com/g/200/200",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+});
+
 function App() {
+  const [ user, setUser ] = useState();
+  const [network, setNetwork] = useState("");
+  
+  // FCL Integration
+  const logIn = () => {
+    fcl.authenticate();
+  };
+  const logOut = () => {
+      fcl.unauthenticate();
+  };
+  useEffect(() => {
+    // This listens to changes in the user objects
+    // and updates the connected user
+    fcl.currentUser().subscribe(setUser);
+  }, [])
+
+  // Lilico Integration
+  useEffect(()=>{
+    // This is an event listener for all messages that are sent to the window
+    window.addEventListener("message", d => {
+    // This only works for Lilico testnet to mainnet changes
+      if(d.data.type==='LILICO:NETWORK') setNetwork(d.data.network)
+    })
+  }, [])
+
+  const RenderLogin = () => {
+    return (
+      <div>
+        <button className="cta-button button-glow" onClick={() => logIn()}>
+          Log In
+        </button>
+      </div>
+    );
+  };
+
+  const RenderLogout = () => {
+    if (user && user.addr) {
+      return (
+        <div className="logout-container">
+          <button className="cta-button logout-btn" onClick={() => logOut()}>
+            ❎ {"  "}
+            {user.addr.substring(0, 6)}...{user.addr.substring(user.addr.length - 4)}
+          </button>
+        </div>
+      );
+    }
+    return undefined;
+  };
 
   return (
     <div className="App">
+      <RenderLogout />
       <div className="container">
         <div className="header-container">
           <div className="logo-container">
@@ -19,10 +82,13 @@ function App() {
           </div>
           <br/>
           <center>
-            <img height="600" width="400" src={splashScreen} alt="grand theft office loading screen"/>
+            <img height="500" width="350" src={splashScreen} alt="grand theft office loading screen"/>
           </center>
           <p className="sub-text">Can you survive the streets of Scranton?</p>
         </div>
+
+        {/* If not logged in, render login button */}
+        {user && user.addr ? "Wallet connected!" : <RenderLogin />}
 
         <div className="footer-container">
             <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
