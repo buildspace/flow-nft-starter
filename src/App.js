af507e2dc8400be98499baba0b8405cf7deced01
Ousikai <mtfallsVR@protonmail.com>
2022-11-26 15:37:22 -0500

    🎩 Display minted NFTs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import splashScreen from "./assets/gto6.webp"
 
 // Contracts 
 import { mintNFT } from "./cadence/transactions/mintNFT_tx";
+import { getIDs } from "./cadence/scripts/getID_script";
+import { getMetadata } from "./cadence/scripts/getMetadata_script";
 import { getTotalSupply } from "./cadence/scripts/getTotalSupply_script";
 
 // Constants
@@ -26,14 +28,16 @@ fcl.config({
 });
 
 function App() {
-  const [ user, setUser ] = useState();
+  const [ images, setImages ] = useState([])
   // const [network, setNetwork] = useState("");
+  const [ user, setUser ] = useState();
   
   // FCL Integration
   const logIn = () => {
     fcl.authenticate();
   };
   const logOut = () => {
+      setImages([]);
       fcl.unauthenticate();
   };
   const mint = async() => {
@@ -72,6 +76,55 @@ function App() {
     }
   }
 
+  const fetchNFTs = async () => {
+    // Empty the images array
+    setImages([]);
+    let IDs = [];
+    
+    // Fetch the IDs with our script (no fees or signers necessary)
+    try {
+      IDs = await fcl.query({
+        cadence: `${getIDs}`,
+        args: (arg, t) => [
+          arg(user.addr, types.Address), 
+        ],
+      })
+    } catch(err) {
+      console.log("No NFTs Owned")
+    }
+    
+    let _imageSrc = []
+    try{
+      for(let i=0; i<IDs.length; i++) {
+        const result = await fcl.query({
+          cadence: `${getMetadata}`,
+          args: (arg, t) => [
+            arg(user.addr, types.Address), 
+            arg(IDs[i].toString(), types.UInt64),
+          ],
+        })
+        // If the source is an IPFS link, remove the "ipfs://" prefix
+        if (result["thumbnail"].startsWith("ipfs://")) {
+          _imageSrc.push(result["thumbnail"].substring(7))
+          // Add a gateway prefix
+          _imageSrc[i] = "https://nftstorage.link/ipfs/" + _imageSrc[i]
+        }
+        else {
+          _imageSrc.push(result["thumbnail"])
+        }
+      }
+    } catch(err) {
+      console.log(err)
+    }
+    
+    if(images.length < _imageSrc.length) {
+      setImages((Array.from({length: _imageSrc.length}, (_, i) => i).map((number, index)=>
+        <img style={{margin:"10px", height: "150px"}} src={_imageSrc[index]} key={number} alt={"NFT #"+number}
+        />
+      )))
+    }
+  }
+
   // Use Effects
   useEffect(() => {
     // This listens to changes in the user objects
@@ -88,6 +141,20 @@ function App() {
   //   })
   // }, [])
 
+  useEffect(() => {
+    if (user && user.addr) {
+      fetchNFTs();
+    }
+  }
+  , [user]);
+
+  // const RenderGif = () => {
+  //   const gifUrl = user?.addr
+  //       ? "https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy-downsized.gif"
+  //       : "https://i.giphy.com/media/Y2ZUWLrTy63j9T6qrK/giphy.webp";
+  //   return <img className="gif-image" src={gifUrl} height="300px" alt="Funny gif"/>;
+  // };
+  
   // Render Components 
   const RenderLogin = () => {
     return (
@@ -117,6 +184,14 @@ function App() {
         <button className="cta-button button-glow" onClick={() => mint()}>
           Mint
         </button>
+        {images.length > 0 ? 
+          <>
+            <h2>Your NFTs</h2>
+              <div className="image-container">
+                {images}
+              </ div>
+          </> 
+        : ""}
       </div>
     );
   }
@@ -132,8 +207,9 @@ function App() {
           </div>
           <br/>
           <center>
-            <img height="500" width="350" src={splashScreen} alt="grand theft office loading screen"/>
+            <img height="300" width="200" src={splashScreen} alt="grand theft office loading screen"/>
           </center>
+          {/* <RenderGif/> */}
           <p className="sub-text">Can you survive the streets of Scranton?</p>
         </div>
 
--- src/cadence/transactions/mintNFT_tx.js
+++ src/cadence/transactions/mintNFT_tx.js
@@ -1,9 +1,11 @@
 export const mintNFT = 
 `
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-import BottomShot from 0xc04a0e93eeb82145 
-// This remains the same 
+import Dundie from 0xc04a0e93eeb82145 
+
+// Do not change these
 import NonFungibleToken from 0x631e88ae7f1d7c20
+import MetadataViews from 0x631e88ae7f1d7c20
 
 transaction(
   recipient: Address,
@@ -12,20 +14,20 @@ transaction(
   thumbnail: String,
 ) {
   prepare(signer: AuthAccount) {
-    if signer.borrow<&BottomShot.Collection>(from: BottomShot.CollectionStoragePath) != nil {
+    if signer.borrow<&Dundie.Collection>(from: Dundie.CollectionStoragePath) != nil {
       return
     }
 
     // Create a new empty collection
-    let collection <- BottomShot.createEmptyCollection()
+    let collection <- Dundie.createEmptyCollection()
 
     // save it to the account
-    signer.save(<-collection, to: BottomShot.CollectionStoragePath)
+    signer.save(<-collection, to: Dundie.CollectionStoragePath)
 
     // create a public capability for the collection
-    signer.link<&{NonFungibleToken.CollectionPublic}>(
-      BottomShot.CollectionPublicPath,
-      target: BottomShot.CollectionStoragePath
+    signer.link<&{NonFungibleToken.CollectionPublic, MetadataViews.ResolverCollection}>(
+      Dundie.CollectionPublicPath,
+      target: Dundie.CollectionStoragePath
     )
   }
 
@@ -33,12 +35,12 @@ transaction(
   execute {
     // Borrow the recipient's public NFT collection reference
     let receiver = getAccount(recipient)
-      .getCapability(BottomShot.CollectionPublicPath)
+      .getCapability(Dundie.CollectionPublicPath)
       .borrow<&{NonFungibleToken.CollectionPublic}>()
       ?? panic("Could not get receiver reference to the NFT Collection")
 
     // Mint the NFT and deposit it to the recipient's collection
-    BottomShot.mintNFT(
+    Dundie.mintNFT(
       recipient: receiver,
       name: name,
       description: description,
